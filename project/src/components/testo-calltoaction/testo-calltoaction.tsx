e39baaf75d154cb68ab262b2c295bb4ac48d135f
Sarafossati <saravaleriafossati.studia@mohole.it>
2020-06-02 10:20:41 +0200

    modifica testi - Sara

--- project/src/components/testo-calltoaction/testo-calltoaction.tsx
+++ project/src/components/testo-calltoaction/testo-calltoaction.tsx
@@ -12,7 +12,7 @@ export class TestoCalltoaction implements ComponentInterface {
       <div class="background">
         <div class="testi">
           <h1>Online è più facile e veloce</h1>
-          <p>Puoi riattivare la tua polizza, rinnovarla o pagare una rata direttamente dal sito o dall'app ACME.</p>
+          <p>Riattiva o rinnova la tua polizza direttamente dal sito o dall'app ACME.</p>
           <br/>
           <button><a href="#">Scopri di più</a></button>
         </div>
