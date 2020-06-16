1480c3d9af4efd912d9c055c2daba433e97d66da
Sarafossati <saravaleriafossati.studia@mohole.it>
2020-06-16 18:13:03 +0200

    sistemazione media query - Sara

--- project/src/components/container-mobile/container-mobile.tsx
+++ project/src/components/container-mobile/container-mobile.tsx
@@ -10,10 +10,10 @@ export class ContainerMobile implements ComponentInterface {
   render() {
     return (
       <div>
-        <menu-mobile simpleText="Prodotti"></menu-mobile>
-        <menu-mobile simpleText="Preventivo"></menu-mobile>
-        <menu-mobile simpleText="Contatti"></menu-mobile>
-        <menu-mobile simpleText="News"></menu-mobile>
+        <menu-mobile simpleText="Prodotti"><a href="prodotti.html"></a></menu-mobile>
+        <menu-mobile simpleText="Preventivo"><a href="preventivi.html"></a></menu-mobile>
+        <menu-mobile simpleText="Contatti"><a href="contatti.html"></a></menu-mobile>
+        <menu-mobile simpleText="News"><a href="news.html"></a></menu-mobile>
       </div>
     );
   }
